5ad711e922d59c7cda58a1233f3aa5ba57147ddc
gusar <kana9e@gmail.com>
2025-03-28 22:44:46 +0500

    gas reported added

--- other/Messenger/EVM/hardhat.config.ts
+++ other/Messenger/EVM/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig, task } from "hardhat/config";
 import '@openzeppelin/hardhat-upgrades';
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter"
 import "./tasks/balance"
 import "./tasks/getBlock"
 import "./tasks/getChainIds"
@@ -16,81 +17,84 @@ import "./tasks/sendMessage"
 import "./tasks/estimateFee"
 
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-        {
-            version: "0.8.24",
-            settings: {
-                optimizer: {
-                    enabled: true,
-                    runs: 200,
+    gasReporter: {
+        enabled: true
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.24",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                    viaIR: true,
                 },
-                viaIR: true,
             },
-        },
-    ],
-  },
-  networks: {
-    hardhat: {
-        // @ts-ignore
-        urlParsed: "http://localhost:8545",
-        chainId: 31337
+        ],
     },
-    localhost: {
-        // @ts-ignore
-        urlParsed: "http://localhost:8545",
-        chainId: 31337
-    },
-    teib: {
-        url: "https://evm-testnet.entangle.fi",
-        accounts: {
-            mnemonic: "",
+    networks: {
+        hardhat: {
+            // @ts-ignore
+            urlParsed: "http://localhost:8545",
+            chainId: 31337
         },
-        chainId: 33133
-    },
-    ethereum_sepolia: {
-        url: "https://ethereum-sepolia-rpc.publicnode.com",
-        accounts: {
-            mnemonic: "",
+        localhost: {
+            // @ts-ignore
+            urlParsed: "http://localhost:8545",
+            chainId: 31337
         },
-        chainId: 11155111
-    },
-    polygon_amoy: {
-        url: "https://rpc-amoy.polygon.technology",
-        accounts: {
-            mnemonic: ""
+        teib: {
+            url: "https://evm-testnet.entangle.fi",
+            accounts: {
+                mnemonic: "",
+            },
+            chainId: 33133
         },
-        chainId: 80002
-    },
-    mantle_sepolia: {
-        url: "https://rpc.sepolia.mantle.xyz",
-        accounts: {
-            mnemonic: ""
+        ethereum_sepolia: {
+            url: "https://ethereum-sepolia-rpc.publicnode.com",
+            accounts: {
+                mnemonic: "",
+            },
+            chainId: 11155111
         },
-        chainId: 5003
-    },
-    base_sepolia: {
-        url: "https://sepolia.base.org",
-        accounts: {
-            mnemonic: ""
+        polygon_amoy: {
+            url: "https://rpc-amoy.polygon.technology",
+            accounts: {
+                mnemonic: ""
+            },
+            chainId: 80002
         },
-        chainId: 84532
-    },
-    sonic_blaze: {
-        url: "https://rpc.blaze.soniclabs.com",
-        accounts: {
-            mnemonic: ""
+        mantle_sepolia: {
+            url: "https://rpc.sepolia.mantle.xyz",
+            accounts: {
+                mnemonic: ""
+            },
+            chainId: 5003
         },
-        chainId: 57054
-    },
-    avalanche_fuji: {
-        url: "https://avalanche-fuji.drpc.org",
-        accounts: {
-            mnemonic: ""
+        base_sepolia: {
+            url: "https://sepolia.base.org",
+            accounts: {
+                mnemonic: ""
+            },
+            chainId: 84532
         },
-        chainId: 43113
+        sonic_blaze: {
+            url: "https://rpc.blaze.soniclabs.com",
+            accounts: {
+                mnemonic: ""
+            },
+            chainId: 57054
+        },
+        avalanche_fuji: {
+            url: "https://avalanche-fuji.drpc.org",
+            accounts: {
+                mnemonic: ""
+            },
+            chainId: 43113
+        }
     }
-  }
 };
 
 export default config;
